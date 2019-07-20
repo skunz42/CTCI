3f6e6ff9040759d00e91574c47c8166b8c12cd71
skunz42 <skunz598@gmail.com>
2019-07-20 16:29:36 -0400

    Started Ch 2

---
+++ Ch 2/LLC.py
@@ -0,0 +1,8 @@
+# CTCI Chapter 2
+
+from node import Node
+
+class LLC:
+    def __init__(self):
+        self.head = None
+        self.tail = None
---
+++ Ch 2/node.py
@@ -0,0 +1,6 @@
+# CTCI Chapter 2
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
