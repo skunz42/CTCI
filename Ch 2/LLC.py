fef8fa08d8b3773ad7e2c661e2daeb551665d5be
skunz42 <skunz598@gmail.com>
2019-07-21 15:15:36 -0400

    Added remove

--- Ch 2/LLC.py
+++ Ch 2/LLC.py
@@ -6,3 +6,34 @@ class LLC:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def insert(self, value):
+        n = Node(value)
+
+        if self.head is None:
+            self.head = n
+            self.tail = n
+        else:
+            self.tail.next = n
+            self.tail = n
+
+    def remove(self, value):
+        curr = self.head
+        prev = self.head
+        while curr is not None:
+            if curr.data == value:
+                if curr == self.head:
+                    self.head = curr.next
+                    #prev = curr
+                else:
+                    prev.next = curr.next
+                    curr = prev.next
+            else:
+                prev = curr
+                curr = curr.next
+
+    def printLL(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.data)
+            curr = curr.next
---
+++ Ch 2/driver.py
@@ -0,0 +1,16 @@
+# CTCI Ch 2
+
+from node import Node
+from LLC import LLC
+
+def main():
+    l = LLC()
+    l.insert(2)
+    l.insert(4)
+    l.insert(3)
+    l.printLL()
+    l.remove(2)
+    l.remove(3)
+    print('-----------------')
+    l.printLL()
+main()
