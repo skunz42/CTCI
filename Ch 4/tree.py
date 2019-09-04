60c5ca057b04b7b7585e171f7dce897b4db9e2de
skunz42 <skunz598@gmail.com>
2019-09-03 22:36:53 -0400

    Added BST delete

--- Ch 4/tree.py
+++ Ch 4/tree.py
@@ -42,33 +42,24 @@ class Tree:
         self.postorder(node.right)
         print(node.data)
 
-    '''def delete(self, data, root):
-        if not root:
-            return root
+    def delete(self, data, node):
+        if not node:
+            return node
 
-        if data == root.data and root.left:
-            print("g: " + str(self.data))
-            self = self.left
-            print("g: " + str(self.data))
-            return root.left
-        elif data == root.data and root.right:
-            return root.right
-
-        if root.data > data:
-            root.left = self.delete(data, root.left)
-        elif root.data < data:
-            root.right = self.delete(data, root.right)
+        if node.data > data:
+            node.left = self.delete(data, node.left)
+        elif node.data < data:
+            node.right = self.delete(data, node.right)
         else:
-            if not root.right:
-                return root.left
-            if not root.left:
-                return root.right
-
-            temp = root.right
+            if not node.right:
+                return node.left
+            if not node.left:
+                return node.right
+            temp = node.right
             min = temp.data
             while temp.left:
                 temp = temp.left
                 min = temp.data
-            root.data = min
-            root.right = self.delete(root.right, root.data)
-        return root'''
+            node.data = min
+            node.right = self.delete(node.data, node.right)
+        return node
